386236bc1ef0a0de78dad6625d37f8cf5f129dc8
Elig-Arsee <arse.eligc@gmail.com>
2022-12-09 17:39:35 -0300

    Pronto_TCC_Elig
    Atualização de scripts, principalmente o do teste qui-quadrado de aderência

--- qui-quad_aderencia.R
+++ qui-quad_aderencia.R
@@ -2,15 +2,17 @@
 
 levels(Data2018$Q12P2.B_Atitude_Turno_2)
 
+options(scipen = 999)
+
 quiqua <- chisq.test(x = table(Data2018$Q12P2.B_Atitude_Turno_2),
-                     p = c(0.711, 0.212, 0.077))
+                     p = c(0.78, 0.21, 0.01))
 
 #2018, conforme site TSE.
-#nominais: 0,7117
-#abstenção 0,2129
-#Voto nulo 0,0584
+#nominais: 0,6793
+#abstenção 0,2199
+#Voto nulo 0,0743
 #voto em branco 0,0193
-#Voto branco e nulo 0.077
+#Voto branco e nulo 0.1008
 
 quiqua
 
@@ -31,7 +33,8 @@ quiqua$stdres#resídos são significativos fora da faixa -1,96 + 1,96
 
 library(fstat)
 
-fstat::graf_obs_esp_ader(Data2018, v1 = Q12P2.B_Atitude_Turno_2,
-                         props = c(0.711, 0.212, 0.077), cor = paleta)
+QuiQua <- fstat::graf_obs_esp_ader(Data2018, v1 = Q12P2.B_Atitude_Turno_2,
+                         props = c(0.78, 0.21, 0.01), cor = paleta)
 
-ggsave("graf12.png", width = 6, height = 4.5, dpi = 400)
+
+ggsave("EligQUIQUAD.png", width = 6, height = 4.5, dpi = 400)
